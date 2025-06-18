627732888186eaf991e2deba96098f0a39f9a55e
Pascal Brand <pascal.brand38@gmail.com>
2025-06-18 10:48:47 +0200

    astro-swiper: thumbnail early version
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 // Copyright (c) Pascal Brand
 // MIT License
 
-import type { SwiperOptions } from 'swiper/types'
+import type { Swiper, SwiperOptions } from 'swiper/types'
 
 import type { HTMLAttributes } from 'astro/types'
 
 export interface AstroSwiperType extends HTMLAttributes<"div"> {
   options?: SwiperOptions,
+  uniqueClass?: string,
+  thumbOf?: string,
 }
 
 export { default as Swiper } from './components/Swiper.astro'
@@ -16,3 +18,6 @@ export { default as SwiperPagination } from './components/SwiperPagination.astro
 export { default as SwiperScrollbar } from './components/SwiperScrollbar.astro'
 export { default as SwiperSlide } from './components/SwiperSlide.astro'
 export { default as SwiperWrapper } from './components/SwiperWrapper.astro'
+
+export const useSwiper: { [className: string] : Swiper; } = {};
+export const useOptions: { [className: string] : SwiperOptions; } = {};
