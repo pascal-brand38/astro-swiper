ad8d8fdedb9dbb219b12a3100dceff3bdda422ec
Pascal Brand <pascal.brand38@gmail.com>
2026-07-26 18:36:32 +0200

    test: retry set to 3
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- demo/playwright.config.ts
+++ demo/playwright.config.ts
@@ -6,6 +6,8 @@ import { defineConfig, devices } from '@playwright/test'
 export default defineConfig({
   testDir: './tests/playwright',
   fullyParallel: true,
+  retries: 3,
+  failOnFlakyTests: false,
   // forbidOnly: !!process.env.CI,
   // retries: process.env.CI ? 2 : 0,
   // workers: process.env.CI ? 1 : undefined,
